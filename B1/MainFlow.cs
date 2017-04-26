43f27efe1a34c5ba91ae61835cc08b6f55e2c599
TOKISHIRAZU <ttlatex@yahoo.co.jp>
2017-04-26 14:28:54 +0900

    B1A1

---
+++ B1/MainFlow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B1
+{
+    /// <summary>
+    /// メインフローです
+    /// </summary>
+    public class MainFlow
+    {
+
+
+        public void start()
+        {
+
+        }
+
+        private void Init()
+        {
+
+        }
+
+        private void MainProcess()
+        {
+
+        }
+    }
+}
