68bdae4d10bda6469beed9e69892511de593229c
TOKISHIRAZU <ttlatex@yahoo.co.jp>
2017-12-07 22:12:29 +0900

    使用しないクラスを削除

--- Jig/Common/RetryJig.cs
+++ Jig/Common/RetryJig.cs
@@ -40,5 +40,35 @@ namespace Jig.Common
 
             ExceptionDispatchInfo.Capture(exStack).Throw();
         }
+
+        /// <summary>
+        /// メソッドのリトライ実行を行います
+        /// </summary>
+        /// <param name="someMethod">リトライ実行を行うメソッド</param>
+        /// <param name="retryCount">リトライ回数</param>
+        /// <param name="retryWaitCount">リトライ待機秒数</param>
+        public static void Retry(Action someMethod, int retryCount, int retryWaitCount, Action<Exception> onError)
+        {
+            Exception exStack = null;
+
+            foreach (var i in Enumerable.Range(1, retryCount + 1))
+            {
+                try
+                {
+                    someMethod();
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    exStack = exStack ?? ex;
+
+                    onError(ex);
+                }
+
+                Thread.Sleep(retryWaitCount * 1000);
+            }
+
+            ExceptionDispatchInfo.Capture(exStack).Throw();
+        }
     }
 }
